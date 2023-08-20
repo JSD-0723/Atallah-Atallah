092dc88c42fa482fd02d04a8fd77a7c889900065
Atallah <Atallah.192000@gmail.com>
2023-08-20 20:22:51 +0300

    Made Dark Mode Feature

---
+++ actions.js
@@ -0,0 +1,23 @@
+// Dark Theme
+var darkMode = document.getElementById("dark-mode");
+darkMode.onclick = function () {
+    document.body.classList.toggle("dark-theme");
+
+    var icon = document.getElementById("moon")
+    if (document.body.classList.contains("dark-theme")) {
+        icon.name = "sunny-outline";
+    } else {
+        icon.name = "moon-outline";
+    }
+}
+
+// Show & Hide Favourites Card Bar
+function toggle() {
+    var favCardBar = document.getElementById('hide-fav-bar')
+
+    if (favCardBar.style.display === 'none') {
+        favCardBar.style.display = 'block';
+    } else {
+        favCardBar.style.display = 'none';
+    }
+}   
